82ac655fbd2f39f7b210bb72c2cc0d495274897e
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-17 23:01:37 +0530

    finished project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import html2canvas from "html2canvas";
 
 import "./styles.css";
-import image from "./kanye/3.jpg";
-import html2canvas from "html2canvas";
+import image from "./kanye/kanyebg.jpg"
 import Quote from "./components/Quote";
 import Button from "./components/Button";
 
 export default function App() {
-  const printRef = React.useRef();
-  const [quoteText, setQuoteText] = useState("Yank the compass!");
+  const printRef = useRef(null);
+  const [quoteText, setQuoteText] = useState("If you have the opportunity to play this game called life, you have to appreciate every moment. A lot of people don't appreciate their moment until it's passed");
 
-  function handleQuote() {
-    fetch("https://api.kanye.rest")
-      .then((response) => response.json())
-      .then((json) => {
-        setQuoteText(json.quote);
-      });
-  }
+  const handleQuote = async () => {
+    const response = await fetch("https://api.kanye.rest");
+    const data = await response.json();
+    console.log(data);
+    setQuoteText(data.quote);
+    {/**Fetch the data from the API -> Store it in data as JSON -> set the value of quoteText state param with data.quote */}
+  };
 
   const handleDownload = async () => {
-    const element = printRef.current;
-    const canvas = await html2canvas(element, {
+    const canvas = await html2canvas(printRef.current, {
       allowTaint: false,
       useCORS: true
     });
 
-    const data = canvas.toDataURL("image/jpg");
+    const data = canvas.toDataURL("image/jpeg");
     const link = document.createElement("a");
 
     if (typeof link.download === "string") {
       link.href = data;
-      link.download = "image.jpg";
+      link.download = "quote.jpg";
 
       document.body.appendChild(link);
       link.click();
@@ -40,12 +39,17 @@ export default function App() {
     }
   };
 
+
   return (
+    //wrapping the div with Kanye image as background
     <div
       className="App flex-col"
-      style={{ backgroundImage: `url(${image})`, backgroundSize: "80%" }}
+      style={{ backgroundImage: `url(${image})`, backgroundSize: "cover" }}
     >
-      <Quote quoteText={quoteText} printRef={printRef} />
+      {/**Have just 2 components here, that is the quoting of the text and the Handle quotes buttons */}
+      <div className="center-container">
+        <Quote quoteText={quoteText} printRef={printRef} />
+      </div>
       <Button handleQuote={handleQuote} handleDownload={handleDownload} />
     </div>
   );
